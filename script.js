7e15c4411b5f948ad5bf530d4488c1325a31f98d
yujeiry <43036901+yujeiry@users.noreply.github.com>
2019-03-26 08:59:07 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,48 @@
+var a;
+$(".button1").click(function(){
+   a = $(".name").val();
+   console.log(a);
+   $(".nameval").html(a);
+   });
+   
+   var b;
+$(".button1").click(function(){
+   b = $(".age").val();
+   console.log(b);
+   $(".ageval").html(b);
+   });
+  
+  var c;
+$(".button1").click(function(){
+   c = $(".job").val();
+   console.log(c);
+   $(".jobval").html(c);
+   });
+
+$("button1").click(function(){
+    $(".world").show();
+});
+
+$("button1").click(function(){
+    $(".inputs-box").hide();
+    $(".btmhide").hide();
+});
+
+$(".button1").click(function(){
+    c=$(".job").val();
+    console.log(c);
+    $(".jobval").html(c);
+});
+
+$(".button1").click(function(){
+    $(".world").show();
+});
+
+$(".buton1").click(function(){
+    $(".inputs-box").hide();
+    $(".btmhide").hide();
+});
+
+
+   
+   
